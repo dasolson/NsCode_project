99bee491f7b6ebe578e9012ec2bea3aac7610aae
dasolson <you@example.com>
2025-12-15 17:24:44 +0900

    20251215

--- food_pinia/src/App.vue
+++ food_pinia/src/App.vue
@@ -1,15 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+  <HeaderCont></HeaderCont>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderCont from './components/HeaderCont.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderCont
   }
 }
 </script>
---
+++ food_pinia/src/components/HeaderCont.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link class="navbar-brand" to="/">Vuex Router</router-link>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><router-link to="/">Home</router-link></li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">맛집 <span
+                            class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><router-link to="/food/find">맛집검색</router-link></li>                        
+                    </ul>
+                </li>
+                <li><router-link to="/youtube/find">동영상 검색</router-link></li>                
+            </ul>
+        </div>
+    </nav>
+</template>
+<script setup>
+/*
+    vuex에 대한 라이브러리 : pinia
+                                                            vuex            pinia
+    state     : 공유하는 데이터                                o               o
+    mutation  : state에 값을 저장                              o               x
+                동기적 처리
+    actions   : 사용자 요청에 따른 처리                         o               o
+    commit    : action에서 처리 결과를 mutation으로 전송     commit('함수')   this.state = 값
+    namespace : 독립적인 모듈 생성                              o               x
+                                                                                | redux
+
+        1. 사용자 요청
+            dispatch
+        2. actions에 있는 함수 호출
+            commit
+        3. actions에 mutation 함수
+        4. state에 저장
+        5. 처리 된 결과값 읽기
+            dispatch를 이용해서 읽어 온다
+
+        => 기반 : 자바스크립트 = 문법
+                    .js , .jsx => ts, tsx
+        => 스프링 : 자바문법        
+        ------------------------------------ 라이브러리 함수를 익힌다
+
+*/
+</script>
+<style scoped>
+
+</style>
